44ee5d9195fb513811fe6a59a3cd238008b4e823
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-02-01 03:06:53 +0100

    add front-end url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.enableCors({
-    origin: ['http://localhost:4200', 'https://vercel-angular-test-rosy.vercel.app'],
+    origin: ['http://localhost:4200', 'https://https://quotesfrontend.vercel.app/home'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });

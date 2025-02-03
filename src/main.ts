47b6f48deea784ae5f60f375d9971629cfec7303
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-02-03 10:49:22 +0100

    update urls

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.enableCors({
-    origin: ['http://localhost:4200', 'https://https://quotesfrontend.vercel.app'],
+    origin: ['http://localhost:4200', 'https://https://quotesfrontend.vercel.app',,'quotesfrontend-marcin-mierzwas-projects.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });

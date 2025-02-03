efa00e1b88b2fbc36a262d02a0bdf887792ab217
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-02-03 11:25:59 +0100

    update vercejson

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,6 @@ async function bootstrap() {
       'https://quotesfrontend-marcin-mierzwas-projects.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    exposedHeaders: ['Access-Control-Allow-Origin'],
     });await app.listen(process.env.PORT ?? 3000);}
 bootstrap();

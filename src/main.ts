3a89411dc4614be4b9d8c2b431d9b1ca45ca1719
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-03-12 15:09:40 +0100

    update urls

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
+  const clientUrls = [
+    configService.get<string>('DATASOURCE_PROD_CLIENT_URL'),
+    configService.get<string>('DATASOURCE_DEV_CLIENT_URL'),
+  ].filter(Boolean); 
+
   app.enableCors({
-    origin: [
-      'https://quotesfrontend.vercel.app',
-      'http://localhost:4200',
-    ],
+    origin: clientUrls,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    });await app.listen(process.env.PORT ?? 3000);}
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
 bootstrap();

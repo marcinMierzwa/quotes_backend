766b2d3e5d554e3e3a90ae23809a6341950d735e
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-04-08 00:54:51 +0200

    update cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import cookieParser from 'cookie-parser';
-
+import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

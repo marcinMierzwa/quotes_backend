0883ca41b804cc95d895b7aa31a0bf6146987e12
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-04-07 21:17:09 +0200

    update cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT');
+  const port = configService.get<number>('PORT') || 3000;
 
   const clientUrls = [
     configService.get<string>('DATASOURCE_PROD_CLIENT_URL'),
